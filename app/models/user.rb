481ef13c557e8a7ff495aa4d5f362e6ad5669f89
Ryu Sato <ryu@weseek.co.jp>
2019-06-18 03:26:42 +0900

    remove unused option from users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :ldap_authenticatable, :registerable,
-    :recoverable, :rememberable,
-    :timeoutable
-    # LDAP ユーザ作成時に validation を行わないよう無効化する
-    # :validatable
+  devise :ldap_authenticatable, :rememberable, :timeoutable
 
   validates :username, presence: true, uniqueness: true
 
