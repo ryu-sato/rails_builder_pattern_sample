9df3822ba8795718b1b4c9ae1516752a4c37629e
Ryu Sato <ryu@weseek.co.jp>
2019-06-30 19:05:29 +0900

    コメントを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,6 @@
 class User < ApplicationRecord
   rolify
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :ldap_authenticatable, :timeoutable
 
   after_create :assign_default_role
